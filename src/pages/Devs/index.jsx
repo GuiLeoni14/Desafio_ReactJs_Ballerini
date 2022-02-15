f7b3e1b59e9e8ce9c321291252b2e8fc59dd4b8c
GuiLeoni14 <guileone167@gmail.com>
2022-02-15 19:31:10 -0200

    ajustando delete devs

--- src/pages/Devs/index.jsx
+++ src/pages/Devs/index.jsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useRef, useState } from 'react';
 import DevsProvider from '../../context/DevsContext';
 import { DevsContext } from '../../context/DevsContext/context';
-import { loadDevs, addDevs } from '../../context/DevsContext/actions';
+import { loadDevs, addDevs, deleteDevs } from '../../context/DevsContext/actions';
 import Form from './Form';
 export default function Devs() {
     const isMounted = useRef(true);
@@ -13,19 +13,26 @@ export default function Devs() {
         loadDevs(devsDispatch);
     }, [devsDispatch]);
 
-    const handleAddDevs = (values) => {
+    const createDevs = (values) => {
         addDevs(devsDispatch, values);
-        loadDevs(devsDispatch); // verificar depois
+        // loadDevs(devsDispatch);
+    };
+
+    const deleteDev = (id) => {
+        console.log(id);
+        deleteDevs(devsDispatch, id);
     };
 
     return (
         <>
             Devs:
             {devs.map((dev) => (
-                <h1 key={dev.id}>{dev.name}</h1>
+                <h1 key={dev.id} onClick={() => deleteDev(dev.id)}>
+                    {dev.name}
+                </h1>
             ))}
             <button onClick={() => addDevs(devsDispatch)}>Adicionar</button>
-            <Form handleSubmit={handleAddDevs} />
+            <Form handleSubmit={createDevs} />
         </>
     );
 }
