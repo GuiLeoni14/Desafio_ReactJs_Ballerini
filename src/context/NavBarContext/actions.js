c8e92e04e9b57a65def91eca40e9090d6df6533d
GuiLeoni14 <guileone167@gmail.com>
2022-03-02 13:28:53 -0300

    ajustando search na home

--- src/context/NavBarContext/actions.js
+++ src/context/NavBarContext/actions.js
@@ -2,6 +2,9 @@ import * as types from './types';
 export const changeSearch = (dispatch) => {
     dispatch({ type: types.VISIBLE, payload: true });
 };
+export const removeSearch = (dispatch) => {
+    dispatch({ type: types.REMOVE_VISIBLE, payload: false });
+};
 export const devSearch = (dispatch, values) => {
     if (!values[0]) {
         dispatch({ type: types.SEARCH, payload: [{ name: 'sem' }] });
